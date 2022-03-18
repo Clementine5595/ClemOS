72b2754b9d7b9e0bc96fc8fa1abb59a6dcd35ea3
Clementine <clementine11@vk.com>
2022-03-18 21:01:19 +0300

    ...

--- build.bat
+++ build.bat
@@ -9,12 +9,12 @@ set GCC=C:\Users\Clementine\Desktop\123321\bin\i686-elf-gcc.exe
 
 echo Now assembling, compiling, and linking your kernel:
 
-%GCC% -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I include -c -o %FOL%bin/main.o %FOL%kernel/main.c
+%GCC% -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -c -o %FOL%bin/main.o %FOL%main.c -nostdlib
 
-%NAS% -f elf32 -o %ASM%bin\boot.o %ASM%boot.asm
+%NAS% -f elf32 -o %FOL%bin\start.o %ASM%start.asm
 rem Remember this spot here: We will add 'gcc' commands here to compile C sources
 rem This links all your files. Remember that as you add *.o files, you need to
 rem add them after start.o. If you don't add them at all, they won't be in your kernel!
-%LDS% -m elf_i386 -T %ASM%link.ld -o %BIN%kernel.bin %ASM%bin\boot.o
+%LDS% -m elf_i386 -T %ASM%link.ld -o %BIN%kernel.bin %BIN%start.o
 echo Done!
 pause
