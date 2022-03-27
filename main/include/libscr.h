1e70f167e177cb387b3adba645b388b4342f8bb8
Clementine <clementine11@vk.com>
2022-03-27 23:32:53 +0300

    NU NAHUI...

--- main/include/libscr.h
+++
@@ -1,7 +0,0 @@
-extern void cls();
-extern void putch(unsigned char c);
-extern void puts(unsigned char *str);
-extern void settextcolor(unsigned char forecolor, unsigned char backcolor);
-extern void init_video();
-extern void move_csr(void);
-extern void scroll(void);
--- main/modules/kernel.c
+++ main/modules/kernel.c
@@ -1,17 +1,7 @@
-// GCC provides these header files automatically
-// They give us access to useful things like fixed-width types
-#include <libscr.h>
-#include <keyboard.h>
-
-int alive = 1;
-
 void main()
 {
 	kb_init();
 	init_video();
 	puts("Hello, World!\n");
 	puts("Welcome to the kernel.\n");
-	while(alive) {
-		keyboard_handler_main();
-	}
 }
